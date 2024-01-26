d8b306d70c1973971199ecbe4d463a3bd8a431c9
Byeonghyeon <problemp123@gmail.com>
2024-01-26 12:10:20 +0900

    type

--- src/app/Search.tsx
+++ src/app/Search.tsx
@@ -1,45 +1,14 @@
 "use client"
 import axios from "axios"
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
+import { Place } from "./Type";
 
-type Place = {
-    addr1: string,
-    addr2: string,
-    areacode: string,
-    booktour: string,
-    cat1: string,
-    cat2: string,
-    cat3: string,
-    contentid: string,
-    contenttypeid: string,
-    createdtime: string,
-    firstimage: string,
-    firstimage2: string,
-    cpyrhtDivCd: string,
-    mapx: string,
-    modifiedtime: string,
-    sigungucode: string,
-    tel: string,
-    title: string
-}
+
 
 
 const Search:React.FC = () => {
     const [travelPlace, setTravelPlace] = useState<Place[] | null>(null);
     const [keyword, setKeyword] = useState("");
-
-    // useEffect(() => {
-    //     axios.post("http://localhost:8080/", {
-    //         keyword: "강원"
-    //     })
-    //     .then((res) => {
-    //         const data = res.data;
-    //         console.log(data);
-    //     })
-    //     .catch((err) => {
-    //         console.error(err);
-    //     })
-    // }, []);
 
     const btnClick = () => {        
         axios.post("http://localhost:8080/", {

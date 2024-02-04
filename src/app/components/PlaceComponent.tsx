59a495094bdf5c4e15db6d75d67740b0103e5293
Byeonghyeon <problemp123@gmail.com>
2024-02-04 17:51:42 +0900

    place component

---
+++ src/app/components/PlaceComponent.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Place } from "../Type";
+
+type Props = {
+    place: Place
+}
+
+const PlaceComponent:React.FC<Props> = (props: Props) => {
+    return(
+        <></>
+    );
+}
+
+export default PlaceComponent;
--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,7 +1,6 @@
-"use client"
 import { TravelPlaceAtom } from "@/Recoil/RecoilContext";
-import { useEffect } from "react";
 import { useRecoilValue, useRecoilValueLoadable } from "recoil";
+import PlaceComponent from "../components/PlaceComponent";
 
 export default function Loading() {
     const travelPlace = useRecoilValue(TravelPlaceAtom);
@@ -11,12 +10,19 @@ export default function Loading() {
 
     return (
         <div className="flex flex-col items-center justify-center text-center min-h-screen bg-gray-100">
-            {travelPlace.map((place, index) => (
-                <div key={index}>
-                    <p>Key: {place.key}</p>
-                    <p>Count: {place.count}</p>
-                </div>
-            ))}
+            <div>
+                <p>당신께 추천하는 여행지는...</p>
+                <PlaceComponent place={travelPlace[0]} />
+            </div>
+            <div>
+                <p>이런 여행지는 어때요?</p>
+                {travelPlace.slice(1).map((place) => (
+                    <div key={place.count}>
+                        <PlaceComponent place={place} />
+                    </div>
+                ))}
+            </div>
+            
         </div>
     );
 }

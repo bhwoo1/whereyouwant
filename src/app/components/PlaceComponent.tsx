65f852ba6e299f9ae1ad1e86186d6c4ba240900b
Byeonghyeon <problemp123@gmail.com>
2024-02-07 14:54:41 +0900

    PlaceComponent

--- src/app/components/PlaceComponent.tsx
+++ src/app/components/PlaceComponent.tsx
@@ -12,16 +12,19 @@ const PlaceComponent: React.FC<Props> = (props: Props) => {
     const areaJsonTyped = areaJson as AreaJsonType;
 
     return(
-        <div className="bg-gray-100 p-4 rounded-md shadow-md">
-            <p className="text-lg font-bold">{areaJsonTyped[place.areacode]?.name}</p>
-            <p className="text-lg">{areaJsonTyped[place.areacode]?.sigungucode[place.sigungucode]}</p>
-            <ul className="mt-4">
-                {keywordCountEntries.map(([keyword, count]) => (
-                    <li key={keyword} className="text-base">
-                        {keyword}: {count}
-                    </li>
-                ))}
-            </ul>
+        <div className="bg-gray-100 p-4 rounded-md shadow-md bg-white">
+            <p className="text-lg text-gray-700">{areaJsonTyped[place.areacode]?.name}</p>
+            <p className="text-lg font-bold">{areaJsonTyped[place.areacode]?.sigungucode[place.sigungucode]}</p>
+            <div className="mt-4">
+                <p>키워드 검색 결과</p>
+                <ul>
+                    {keywordCountEntries.map(([keyword, count]) => (
+                        <li key={keyword} className="text-base">
+                            {keyword}: {count}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }

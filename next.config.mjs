877da8ff74b8853d0c04d06093d95068f90b1f2d
Byeonghyeon <problemp123@gmail.com>
2024-02-13 14:41:56 +0900

    netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+const nextConfig = {};
 
-// export default nextConfig;
+export default nextConfig;
 
-const nextConfig = {
-    // 다른 구성 옵션들...
-  
-    // 페이지 사전 렌더링 설정
-    experimental: {
-      prerender: {
-        // prerender-manifest.js 파일을 사용하도록 설정합니다.
-        manifest: true
-      }
-    }
-  };
-  
-  export default nextConfig;

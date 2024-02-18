786cff9136cb766589ec6334ca22b8037d59a09a
Byeonghyeon <problemp123@gmail.com>
2024-02-18 18:01:58 +0900

    recoil-persist

--- src/Recoil/RecoilContext.tsx
+++ src/Recoil/RecoilContext.tsx
@@ -1,15 +1,23 @@
 "use client"
 import { Place } from "@/app/Type";
 import { RecoilRoot, atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
 
 export const KeywordArrayAtom = atom({
     key: "KeywordArrayState",
     default: [] as string[]
 });
 
+
+const { persistAtom } = recoilPersist({
+    key: "sessionStorage",
+    storage: sessionStorage
+});
+
 export const TravelPlaceAtom = atom<Place[]>({
     key: "TravelPlaceState",
-    default: []
+    default: [],
+    effects_UNSTABLE: [persistAtom]
 });
 
 
